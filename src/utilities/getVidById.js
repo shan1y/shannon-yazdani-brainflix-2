8aee5e8655f2105fc2d520e7fac3b09c9062dc1b
Shannon Yazdani <shannonyazdani@gmail.com>
2022-07-14 17:07:16 -0400

    had to change to heroku link in utility file

--- src/utilities/getVidById.js
+++ src/utilities/getVidById.js
@@ -2,5 +2,7 @@ import axios from "axios";
 
 //Get request from API.
 export const getVidById = (videoId) => {
-  return axios.get(`http://localhost:8080/videos/${videoId}`);
+  return axios.get(
+    `https://brainflix-project-api.herokuapp.com/videos/${videoId}`
+  );
 };
