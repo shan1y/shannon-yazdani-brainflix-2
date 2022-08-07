2bacb8ff6802b45d59169ade5da1418682402ec3
Shannon Yazdani <shannonyazdani@gmail.com>
2022-08-07 16:15:04 -0400

    added event listener to form q

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,13 +1,39 @@
 import "./Form.scss";
 import avatar from "../../assets/Images/Mohan-muruge.jpg";
+import React from "react";
+import { render } from "@testing-library/react";
+import ReactDOM from 'react-dom';
 
-function Form({ selectedVideo }) {
+class Form extends React.Component {
+
+selectedVideo = this.props.selectedVideo
+
+componentDidUpdate() {
+  let submitForm = document.querySelector(".form")
+ let foundForm = ReactDOM.findDOMNode(submitForm);
+ foundForm.addEventListener("submit", (e)=>{
+  e.preventDefault()
+  console.log(e.target.form__input.value)
+
+  let comment = {
+    name:"Mohan Minaj",
+    comment:e.target.form__input.value,
+    likes:0,
+    timestamp:Date.now()
+  }
+ })
+  
+
+
+}
+
+render() {
   return (
     <section>
       <div className="form__total-comments">
-        {`${selectedVideo.comments.length} comments`}
+        {`${this.props.selectedVideo.comments.length} comments`}
       </div>
-      <div className="form">
+      <form className="form">
         <div className="form__container">
           <div className="form__left">
             <div className="form__avatar-holder">
@@ -19,15 +45,16 @@ function Form({ selectedVideo }) {
               <p className="form__title">Join the conversation</p>
               <textarea
                 className="form__input"
+                name="form__input"
                 placeholder="Add a new comment"
               ></textarea>
             </div>
-            <button className="form__button">comment</button>
+            <button type="submit" className="form__button">comment</button>
           </div>
         </div>
-      </div>
+      </form>
     </section>
   );
-}
+}}
 
 export default Form;
